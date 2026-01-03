225327e363f8e6a2b47e79fea810e1cb98454372
Christoph Lürig <christoph.luerig@gmail.com>
2026-01-03 11:55:21 +0100

    ternio added

--- BuildAll.sh
+++ BuildAll.sh
@@ -7,20 +7,24 @@ mkdir -p "$OUT"
 
 echo "Building WASM..."
 cargo build -p tic-tac-toe --target wasm32-unknown-unknown --release
+cargo build -p ternio --target wasm32-unknown-unknown --release
 
 echo "Building Server..."
 CARGO_PROFILE_RELEASE_OPT_LEVEL=3 cargo build -p relay-server --release
 
 echo "Copying client files..."
 cp target/wasm32-unknown-unknown/release/tic-tac-toe.wasm "$OUT/"
+cp target/wasm32-unknown-unknown/release/ternio.wasm "$OUT/"
 cp backbone-lib/web/*.js "$OUT/"
-cp games/tic-tac-toe/web/*.* "$OUT/"
+cp games/web/*.* "$OUT/"
+cp games/tic-tac-toe/web/tic-tac-toe.html "$OUT/"
+cp games/ternio/web/ternio.html "$OUT/"
 
 echo "Copying server files..."
 cp target/release/relay-server "$OUT/"
 cp relay-server/GameConfig.json "$OUT/"
 
 echo "Create documentation..."
-cargo doc --no-deps -p backbone-lib -p protocol -p tic-tac-toe -p relay-server --open
+cargo doc --no-deps -p backbone-lib -p protocol -p tic-tac-toe -p ternio -p relay-server --open
 
 echo "Done!"
---
+++ games/ternio/src/board_logic/mod.rs
@@ -0,0 +1,5 @@
+//! Contains the board logic and aboard visualization module extracting the relevant information
+//! and generating the transition information.
+
+pub mod board_and_transition;
+pub mod board_representation;
